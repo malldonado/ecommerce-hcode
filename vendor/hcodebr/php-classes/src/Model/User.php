3788d7a914b9571c711d327bd0c7c52e8aa3fbf6
matheusmaldonadosilva <matheussilvacontat@gmail.com>
2023-04-05 16:55:35 -0300

    update ecommerce_hcode

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -4,38 +4,47 @@ namespace Hcode\Model;
 
 use \Hcode\DB\Sql;
 use \Hcode\Model; //calling the class
+use \Hcode\Mailer;
 
 class User extends Model{
 
     const SESSION = "User";
+    const SECRET = "HcodePhp7_Secret"; //key no public in github, remove after
 
-    public static function login($login, $password) {
-        $sql = new Sql();
+    public static function login($login, $password)
+	{
 
-        $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
-            ":LOGIN"=>$login
-        ));
+		$sql = new Sql();
 
-        if(count($results) === 0) {
-            throw new \Exception('Usuário inexistente ou senha inválida.');
-        }
+		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b ON a.idperson = b.idperson WHERE a.deslogin = :LOGIN", array(
+			":LOGIN"=>$login
+		)); 
 
-        $data = $results[0]; //get index 0 the array the results
+		if (count($results) === 0)
+		{
+			throw new \Exception("Usuário inexistente ou senha inválida.");
+		}
 
-        if(password_verify($password, $data['despassword']) === true) {
-            $user = new User();
-        
-            $user->setData($data);
+		$data = $results[0];
 
-            $_SESSION[User::SESSION] = $user->getValues();
+		if (password_verify($password, $data["despassword"]) === true)
+		{
 
-            return $user;
-        }
-        else {
-            throw new \Exception('Usuário inexistente ou senha inválida.');
-        }
-    
-    }
+			$user = new User();
+
+			$data['desperson'] = utf8_encode($data['desperson']);
+
+			$user->setData($data);
+
+			$_SESSION[User::SESSION] = $user->getValues();
+
+			return $user;
+
+		} else {
+			throw new \Exception("Usuário inexistente ou senha inválida.");
+		}
+
+	}
 
     public static function verifyLogin($inadmin = true) {
         if(!isset($_SESSION[User::SESSION]) || !$_SESSION[User::SESSION] || !(int)$_SESSION[User::SESSION]['iduser'] > 0 || (bool)$_SESSION[User::SESSION]['inadmin'] !== $inadmin) {
@@ -101,11 +110,77 @@ class User extends Model{
         $sql = new Sql();
         
         $sql->query('CALL sp_users_delete(:iduser)', array(
-            ":iduser"->$this->getiduser()
+            ":iduser"=>$this->getiduser()
         ));
     
     }
 
+    public static function getForgot($email) {
+        $sql = new Sql();
+        $results = $sql->select("
+			SELECT *
+			FROM tb_persons a
+			INNER JOIN tb_users b USING(idperson)
+			WHERE a.desemail = :email;
+		", array(
+			":email"=>$email
+		));
+        if(count($results) === 0) {
+            throw new \Exception('Não foi possivel recuperar a senha.');
+        }
+          else {
+            
+              $data = $results[0];
+          
+              $results2 = $sql->select('CALL sp_userspasswordsrecoveries_create(:iduser, :desip)', array(
+                ":iduser"=>$data['iduser'],
+                ":desip"=>$data["REMOTE_ADDR"]
+            ));
+        }
+
+        if(count($results2) === 0) {
+
+            throw new \Exception('Não foi possível recuperar a senha');	
+            
+        }
+        else {
+        
+            $dataRecovery = $results2[0];
+            $code = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, User::SECRET, $dataRecovery['idrecovery'], MCRYPT_MODE_ECB));
+        
+            $link = "https://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+
+            $mailer = new Mailer($data['desemail'], $data['desperson'], 'Redefinir senha da Hcode Store', 'forgot', array(
+                "name"=>$data['desperson'],
+                "link"=>$link
+            ));
+
+            $mailer->send();
+
+            return $data;
+        
+        }
+    }
+
+    public static function validaForgotDecrypt($code){
+        $idrecovery = mcrypt_decrypt(MCRYPT_RIJNDAL_128, User::SECRET, base64_decode($code), MCRYPT_MODE_ECB);
+    
+        $sql = new Sql();
+    
+        $results = $sql->select("SELECT * FROM tb_userspasswordsrecoveries a INNER JOIN tb_users b USIGN(iduser) INNER JOIN tb_persons c USIGN(idperson) WHERE a.idrecovery a.idrecovery = :idrecovery AND a.dtrecovery IS NULL AND DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();", array(
+            ":idrecovery"=>$idrecovery
+        ));
+
+        if(count($results) === 0) {
+            throw new \Exception('Não foi possível recuperar a senha');
+        }
+        else {
+            return $results[0];
+        }
+    }
+
 }
+
+
 
 ?>
